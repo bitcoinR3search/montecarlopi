ea5395e90127ab295c1751ebfda21a66be2a305e
jpcrespo <jp.crespo.vargas@gmail.com>
2020-11-11 19:03:38 -0400

    automatizado

---
+++ MC laplace/auto.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import os
+
+import imagenerator
+import statsBuff
+
+k=100  # Para obtener el numero de bins para formar histogramas
+N=np.logspace(3,5,3,endpoint=True,dtype=np.int64) #Para generar un valor N distanciado log
+
+#generador de los resultados para cada N y k
+
+for i in N:
+	statsBuff.results(i,k)
+
+
+#generador de las imagenes para cada binario
+contenido = os.listdir('binaries/')
+for txt in contenido:
+	imagenerator.dibujar(txt)
+
+#grafica errores
+
+imagenerator.errores(N,k)
+
+
+
+
