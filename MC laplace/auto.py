75984a95221f9217df87f16d376941cb5dce402a
jpcrespo <jp.crespo.vargas@gmail.com>
2020-11-11 21:26:52 -0400

    test jpi

--- MC laplace/auto.py
+++ MC laplace/auto.py
@@ -7,15 +7,20 @@ import imagenerator
 import statsBuff
 
 k=200  # Para obtener el numero de bins para formar histogramas
+N=np.logspace(2,5,20,endpoint=True,dtype=np.int64) #Para generar un valor N distanciado log
+print(N)
 
-N=np.logspace(2,5,3,endpoint=True,dtype=np.int64) #Para generar un valor N distanciado log
+N_jp=N[0:20:2];
+N_cc=N[1:20:2];
+
+
 
 #generador de los resultados para cada N y k
 
-for i in N:
+for i in N_jp:
 	statsBuff.results(i,k)
 
-
+'''
 #generador de las imagenes para cada binario
 contenido = os.listdir('binaries/')
 for txt in contenido:
@@ -27,6 +32,7 @@ for txt in contenido:
 
 imagenerator.errores(N,k)
 
+'''
 
 
 
